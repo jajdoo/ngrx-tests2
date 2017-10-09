ac11d1386d0929b69a1785e45b085c18efdb67e9
nir bar-zvi <jajdoo@gmail.com>
2017-10-09 15:49:50 +0300

    tied feelings feature to ngrx

--- src/app/core/reducers/index.ts
+++ src/app/core/reducers/index.ts
@@ -1,7 +1,19 @@
-import {ActionReducerMap} from '@ngrx/store';
+import {ActionReducerMap, createFeatureSelector, createSelector} from '@ngrx/store';
+import * as fromFeelings from './feelings';
 
 export interface State {
+  feelings: fromFeelings.State;
 }
 
 export const reducers: ActionReducerMap<State> = {
+  feelings: fromFeelings.reducer
 };
+
+// feelings feature
+export const getFeelingsState = createFeatureSelector<fromFeelings.State>('feelings');
+
+// attribute selectors
+export const getHateStas = createSelector(
+  getFeelingsState,
+  fromFeelings.getHateStas
+);
