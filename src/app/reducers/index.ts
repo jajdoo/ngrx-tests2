e77ec89855dc8f926c9f3981b4af6273d36f92ff
nir bar-zvi <jajdoo@gmail.com>
2017-10-09 11:16:22 +0300

    removed empty action reducer map from app/reducers/index

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,16 +1,10 @@
 import { storeFreeze } from 'ngrx-store-freeze';
-import {ActionReducer, ActionReducerMap, MetaReducer} from '@ngrx/store';
+import {ActionReducer, MetaReducer} from '@ngrx/store';
 import {environment} from '../../environments/environment';
 
-// -------------------------------
 export interface State {
 }
 
-export const reducers: ActionReducerMap<State> = {
-};
-// -------------------------------
-
-// -------------------------------
 export function logger(reducer: ActionReducer<State>): ActionReducer<State> {
   return function(state: State, action: any): State {
     console.log('state', state);
@@ -23,6 +17,3 @@ export function logger(reducer: ActionReducer<State>): ActionReducer<State> {
 export const metaReducers: MetaReducer<State>[] = !environment.production
   ? [logger, storeFreeze]
   : [];
-// -------------------------------
-
-// other reducers
