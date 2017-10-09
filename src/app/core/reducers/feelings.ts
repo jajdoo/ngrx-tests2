87d0f7c09e5159a3c491f43fd3e4da7c2786f06e
nir bar-zvi <jajdoo@gmail.com>
2017-10-09 15:47:28 +0300

    added feelings feature reducer

--- src/app/core/reducers/feelings.ts
+++ src/app/core/reducers/feelings.ts
@@ -1,3 +1,5 @@
+import * as Feelings from '../actions/feelings';
+
 export interface State {
   hateStas: boolean;
 }
@@ -8,3 +10,19 @@ const initialState: State = {
 
 // selector functions
 export const getHateStas = (state: State) => state.hateStas;
+
+// reducer function
+export function reducer(state = initialState, action: Feelings.Actions): State {
+  switch (action.type) {
+    case Feelings.HATE_STAS:
+      return {
+        hateStas: true
+      };
+
+    case Feelings.STOP_HATING_STAS:
+      return {
+        hateStas: false
+      };
+  }
+  return state;
+}
